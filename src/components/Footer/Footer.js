35035f4ece7e8aaacc96c9595e47dfd89fb982a1
davibarros <davibarros@hotmail.com>
2022-01-04 08:29:31 -0300

    primeiro commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+
+import React from "react";
+import { Container } from "reactstrap";
+
+import PropTypes from "prop-types";
+
+function Footer(props) {
+  return (
+    <footer className={"footer" + (props.default ? " footer-default" : "")}>
+      <Container fluid={props.fluid ? true : false}>
+        <nav>
+          <ul>
+            
+           
+          </ul>
+        </nav>
+        <div className="copyright">
+          &copy; 2022, Designed by Davi de Barros
+          
+          .
+        </div>
+      </Container>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  default: PropTypes.bool,
+  fluid: PropTypes.bool,
+};
+
+export default Footer;
---
+++ src/routes.js
@@ -0,0 +1,39 @@
+
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import TableList from "views/TableList.js";
+import UserPage from "views/UserPage.js";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "design_app",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  
+  
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "ui-1_bell-53",
+    component: Notifications,
+    layout: "/admin",
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "users_single-02",
+    component: UserPage,
+    layout: "/admin",
+  },
+  {
+    path: "/extended-tables",
+    name: "Table List",
+    icon: "files_paper",
+    component: TableList,
+    layout: "/admin",
+  }
+];
+export default dashRoutes;
